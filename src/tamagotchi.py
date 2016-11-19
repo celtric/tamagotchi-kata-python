e5e343d43cab64a477919f8d0283b1216989117e
Ricard Osorio <celtric@gmail.com>
2016-11-19 13:21:03 +0100

    implement playing

--- src/tamagotchi.py
+++ src/tamagotchi.py
@@ -2,14 +2,26 @@ class Tamagotchi:
 
     def __init__(self):
         self.fullness = 0
+        self.happiness = 0
         self.hungriness = 0
+        self.tiredness = 0
 
     def feed(self):
         self.hungriness -= 1
         self.fullness += 1
+
+    def play(self):
+        self.happiness += 1
+        self.tiredness += 1
 
     def is_hungrier_than(self, another_tamagotchi) -> bool:
         return self.hungriness > another_tamagotchi.hungriness
 
     def is_fuller_than(self, another_tamagotchi) -> bool:
         return self.fullness > another_tamagotchi.fullness
+
+    def is_happier_than(self, another_tamagotchi):
+        return self.happiness > another_tamagotchi.happiness
+
+    def is_more_tired_than(self, another_tamagotchi):
+        return self.tiredness > another_tamagotchi.tiredness
--- tests/acceptance/tamagotchi_test.py
+++ tests/acceptance/tamagotchi_test.py
@@ -5,6 +5,8 @@ from src.tamagotchi import *
 
 class TamagotchiTest(unittest.TestCase):
 
+    # ---[ Feeding ]-------------------------------------------------------------------- #
+
     def test_feeding_it_decreases_its_hungriness(self):
         tamagotchi = self.base_tamagotchi()
         tamagotchi.feed()
@@ -16,6 +18,20 @@ class TamagotchiTest(unittest.TestCase):
         tamagotchi.feed()
 
         self.assertTrue(tamagotchi.is_fuller_than(self.base_tamagotchi()))
+
+    # ---[ Playing ]-------------------------------------------------------------------- #
+
+    def test_playing_with_it_increases_its_happiness(self):
+        tamagotchi = self.base_tamagotchi()
+        tamagotchi.play()
+
+        self.assertTrue(tamagotchi.is_happier_than(self.base_tamagotchi()))
+
+    def test_playing_with_it_increases_its_tiredness(self):
+        tamagotchi = self.base_tamagotchi()
+        tamagotchi.play()
+
+        self.assertTrue(tamagotchi.is_more_tired_than(self.base_tamagotchi()))
 
     # ---[ Helpers ]-------------------------------------------------------------------- #
 
